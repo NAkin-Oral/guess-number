f0b3560534d7202fb858f21cd8e898a540fed4ee
nao <nurettinakinoral@gmail.com>
2022-06-07 17:29:17 +0300

    Had  a bit of help

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const end = document.querySelector('.end');
 const first = document.querySelector('#first');
 const last = document.querySelector('#last');
 
+reBtn.style.display = 'none';
 let continueGame;
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
@@ -15,40 +16,46 @@ let live = 5;
 
 function Guess() {
   console.log(randomNumber);
-  if (guessNumber.value > 100 || guessNumber.value < 0) {
-    alert('Please enter a number between 1 and 100');
+  if (guessNumber.value === '') {
+    alert('Please enter a value');
   } else {
-    if (live > 0) {
-      if (guessNumber.value > randomNumber) {
-        result.innerHTML = 'Your guess is high.';
-        live--;
-        life.innerHTML = `You have ${live} tries left.`;
-        last.innerHTML = guessNumber.value;
-      } else if (guessNumber.value < randomNumber) {
-        result.innerHTML = 'Your guess is low.';
-        live--;
-        life.innerHTML = `You have ${live} tries left.`;
-        first.innerHTML = guessNumber.value;
+    if (guessNumber.value > 100 || guessNumber.value < 0) {
+      alert('Please enter a number between 1 and 100');
+    } else {
+      live--;
+      if (live > 0) {
+        if (guessNumber.value > randomNumber) {
+          result.innerHTML = 'Your guess is high.';
+          life.innerHTML = `You have ${live} tries left.`;
+          last.innerHTML = guessNumber.value;
+        } else if (guessNumber.value < randomNumber) {
+          result.innerHTML = 'Your guess is low.';
+          life.innerHTML = `You have ${live} tries left.`;
+          first.innerHTML = guessNumber.value;
+        }
       } else if (guessNumber.value == randomNumber) {
         end.innerHTML = 'YOU WON!';
         result.innerHTML = '';
-        live--;
         life.innerHTML = `You won with ${live} lives left !`;
         randomValue.innerHTML = randomNumber + ' was the number';
         guessSubmit.disabled = true;
+        reBtn.style.display = 'block';
+      } else if (live == 0) {
+        end.innerHTML = 'YOU LOST!';
+        randomValue.innerHTML = randomNumber + ' was the number';
+        life.innerHTML = `You have ${live} tries left.`;
+        guessSubmit.disabled = true;
+        reBtn.style.display = 'block';
       }
-    } else {
-      end.innerHTML = 'YOU LOST!';
-      randomValue.innerHTML = randomNumber + ' was the number';
-      life.innerHTML = `You have ${live} tries left.`;
-      guessSubmit.disabled = true;
     }
   }
 }
 guessSubmit.addEventListener('click', Guess);
-
-window.addEventListener('load', event => {
-  reBtn.onclick = function () {
-    location.reload(true);
-  };
+guessSubmit.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    Guess;
+  }
 });
+reBtn.onclick = function () {
+  location.reload(true);
+};
